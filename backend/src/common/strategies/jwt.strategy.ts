1eadbc7d5fc4c17180bffa9de4bab2c49a3f935a
increChong <1219832612@qq.com>
2026-03-08 11:47:27 +0800

    feat(backend): add backend/src/common/strategies/jwt.strategy.ts

---
+++ backend/src/common/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from '../../modules/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET', 'secret'),
+    });
+  }
+
+  async validate(payload: { sub: string }) {
+    const user = await this.userService.findById(payload.sub);
+    return user;
+  }
+}
